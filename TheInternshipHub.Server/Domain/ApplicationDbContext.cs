219b372f35b80d6e98140740eab38457dcbe9332
Tudor Suiu <tudorsuiu21@gmail.com>
2024-12-17 19:10:32 +0200

    first commit

---
+++ TheInternshipHub.Server/Domain/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TheInternshipHub.Server.Domain
+{
+    namespace SmartService.Domain
+    {
+        public class ApplicationDbContext : DbContext
+        {
+            public ApplicationDbContext(DbContextOptions options) : base(options)
+            {
+
+            }
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                base.OnModelCreating(modelBuilder);
+            }
+        }
+    }
+}
---
+++ TheInternshipHub.Server/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using TheInternshipHub.Server.Domain.SmartService.Domain;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DatabaseConnection")));
+
+var app = builder.Build();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapFallbackToFile("/index.html");
+
+app.Run();
