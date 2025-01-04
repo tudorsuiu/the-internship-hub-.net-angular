57da9a743e2e90495e94bd2fe97c9b19bea8ae49
Radu Sabina <sabina.radu02@gmail.com>
2025-01-04 20:11:15 +0200

    Saving changes

--- theinternshiphub.client/src/app/add-internship/add-internship.component.ts
+++ theinternshiphub.client/src/app/add-internship/add-internship.component.ts
@@ -2,6 +2,8 @@ import { CommonModule, Location } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ICompany } from '../dtos/ICompany';
+import { UserService } from '../services/user/user.service';
+import { IUser } from '../dtos/IUser';
 
 @Component({
     selector: 'app-add-internship',
@@ -19,7 +21,18 @@ export class AddInternshipComponent {
     positionsAvailable: number | null = null;
     compensation: number | null = null;
 
-    constructor(private location: Location) {}
+    constructor(private location: Location, private userService: UserService) {}
+
+    ngOnInit() {
+        this.userService.getUserById(localStorage.getItem('token')).subscribe(
+            (response: IUser) => {
+                console.log(response);
+            },
+            (error: any) => {
+                console.log(error.message);
+            }
+        );
+    }
 
     navigateBack() {
         this.location.back();
---
+++ theinternshiphub.client/src/app/services/user/user.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IUser } from '../../dtos/IUser';
+import { endpointAPI } from '../../../config';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class UserService {
+    constructor(private http: HttpClient) {}
+
+    getUserById(id: string | null): Observable<IUser> {
+        return this.http.get<IUser>(`${endpointAPI}/api/Users/${id}`);
+    }
+}
